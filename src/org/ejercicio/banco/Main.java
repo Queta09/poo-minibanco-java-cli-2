1f34b2b47ed74b77cbcf193678ebbf036c5efa46
Queta09 <quetacoba@gmail.com>
2025-09-02 18:14:00 +0000

    Actividad Poo: Minibanco inicial

--- src/org/ejercicio/banco/Main.java
+++ src/org/ejercicio/banco/Main.java
@@ -7,5 +7,9 @@ public class Main {
        Cliente cliente1 = new Cliente("David Coba", 25672946);
        CuentaBancaria cuenta1 = new CuentaBancaria(6831531,cliente1,10000);
 
+       Cliente cliente2 = new Cliente("David ku", 29872946);
+       CuentaBancaria cuenta2 = new CuentaBancaria(61531,cliente1,1000);
+
+
     }
 }
